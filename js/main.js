0492e7824c07f90916db6eda621c074b20ee4f24
AjDevLabs <ajdevlabs16@gmail.com>
2022-02-08 20:50:11 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,5 @@ var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 var letterColors = [red, orange, green, blue, purple];
 
-if(2 < 3)
-{
-    bubbleShape = 'square';
-}
-else
-{
-    bubbleShape = 'circle';
-}
 
 bounceBubbles();
